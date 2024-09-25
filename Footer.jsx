c2aa497f5bf8cab80efddef4f8e740bfe6262134
RhoneBryce <20192672@s.ubaguio.edu>
2024-09-25 16:14:10 +0800

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,14 @@
+export default function Footer () {
+    return (
+        <>
+        <footer>
+        <hr />
+        <p>
+            &copy;
+            {new Date().getFullYear()} My color picker website || Written by : Chamos
+        </p>
+        </footer>
+        </>
+    
+    )
+}
---
+++ Header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+    return  (
+        <>
+        <header>
+                <h1> <center>Color Picker: A web Application</center></h1>
+                <hr />
+        </header>
+        </>
+    )
+        
+    
+}
