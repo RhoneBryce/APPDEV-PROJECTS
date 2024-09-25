c5ca31fc28933400875128838857552aeb8ef530
RhoneBryce <20192672@s.ubaguio.edu>
2024-09-25 16:23:05 +0800

    Add files via upload

---
+++ react-counter/src/Footer.jsx
@@ -0,0 +1,14 @@
+export default function Footer () {
+    return (
+        <>
+        <footer>
+        <hr />
+        <p>
+            &copy;
+            {new Date().getFullYear()} My Counter Website || Written by : Chamos
+        </p>
+        </footer>
+        </>
+    
+    )
+}
---
+++ react-counter/src/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+    return  (
+        <>
+        <header>
+                <h1> <center>My Counter Project</center></h1>
+                <hr />
+        </header>
+        </>
+    )
+        
+    
+}
